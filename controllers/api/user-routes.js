969978e31c9b7ef1c246546d2cccc7f373de39d1
ReenaH <Reena_Hunjan@yahoo.com>
2022-08-07 18:22:30 -0400

    add login logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,7 +55,16 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+   .then(dbUserData => {
+  req.session.save(() => {
+    req.session.user_id = dbUserData.id;
+    req.session.username = dbUserData.username;
+    req.session.loggedIn = true;
+
+    res.json(dbUserData);
+  });
+})
+   
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -64,7 +73,6 @@ router.post('/', (req, res) => {
 
 //Login
 router.post('/login', (req, res) => {
- 
   User.findOne({
     where: {
       email: req.body.email
@@ -76,13 +84,34 @@ router.post('/login', (req, res) => {
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
   });
+});
+
+//logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+
 });
 
 
