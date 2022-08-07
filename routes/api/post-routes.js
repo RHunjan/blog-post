e006746a84fd8bf57c24b77bfc7a39e757c0e395
ReenaH <Reena_Hunjan@yahoo.com>
2022-08-07 13:26:47 -0400

    added comment model and routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all posts
 router.get('/', (req, res) => {
@@ -7,6 +7,15 @@ router.get('/', (req, res) => {
     attributes: ['id', 'title', 'post_content', 'created_at'],
     order: [['created_at', 'DESC']], 
     include: [
+         {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+         model: User,
+         attributes: ['username']
+         }
+   },
+     
       {
         model: User,
         attributes: ['username']
@@ -28,6 +37,15 @@ router.get('/:id', (req, res) => {
     },
     attributes: ['id', 'title', 'post_content', 'created_at'],
     include: [
+          {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+         model: User,
+         attributes: ['username']
+         }
+   },
+      
       {
         model: User,
         attributes: ['username']
