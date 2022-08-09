275022343a74f8dffcd100acfeb131da9ad1b917
ReenaH <Reena_Hunjan@yahoo.com>
2022-08-09 19:51:30 -0400

    fixed login

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,12 +6,12 @@ VALUES
 
 INSERT INTO post (title, post_content, user_id, created_at, updated_at)
 VALUES
-('Did you know computers are real', 'It is true that computers are real', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Potatoes that are blue are better', 'Shocking discovery about purple potatoes', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Gratitude', 'There is so very much to be grateful for', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+('Picturing time', 'Oscilloscopes are indispensable instruments in electronics labs. They can display much more than signal paths.', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Do not disturb', 'An increasing number of products have wireless modules. Coexistence and conventional EMC tests ensure interferencefree operation.', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Measuring in the cloud', 'Performing measurements without directly accessing your measuring instruments? An enticing prospect for engineers not only when working from home. A T&M cloud makes it possible.', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO comment (comment_text, user_id, post_id, created_at, updated_at)
 VALUES
-('This is a comment', 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('This is also a comment', 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Look at me I am also a comment', 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+('How interesting', 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Really?', 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Fascinating', 2, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
